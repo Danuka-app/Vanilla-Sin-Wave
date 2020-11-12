b6e52ba58485d25675a5694b8b4099a6af5f52c5
Danuka Ashan <danukaashan886@gmail.com>
2020-11-12 22:28:19 +0530

    corrections

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 /*
  * Wave text
  */
-const startTime = new Date().getTime();
-const waveLetters = 'Your text here';
-const waveTarget = document.getElementsByClassName('animated-wave')[0];
+/*for extra learn*/
+var startTime = new Date().getTime();
+var waveLetters = 'Your text here';
+var waveTarget = document.getElementsByClassName('animated-wave')[0];
 for (let i = 0; i < waveLetters.length; i++) {
     $(`<span class="letter">${ waveLetters[i] }</span>`).appendTo(waveTarget);
 }
 function runWave() {
-    const width = $(window).width();
-    const letterSpacing = width > 900 ? 24 : 16;
-    const elapsed = new Date().getTime() - startTime;
-    const pos = elapsed * 0.05;
+    var width = $(window).width();
+    var letterSpacing = width > 900 ? 24 : 16;
+    var elapsed = new Date().getTime() - startTime;
+    var pos = elapsed * 0.05;
     $('.animated-wave .letter').each(function(index, letter) {
-        const posx = width - (pos - letterSpacing * index) % width;
-        const posy = Math.sin(posx / 40) * 20;
+        var posx = width - (pos - letterSpacing * index) % width;
+        var posy = Math.sin(posx / 40) * 20;
         $(letter).css('left', posx + 'px');
         $(letter).css('top', posy + 'px');
     });
